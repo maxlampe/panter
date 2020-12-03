22d9f6b38a3b58ec3256787c8d16acd8f173b28a
Max Lamparth <max.lamparth@tum.de>
2020-12-03 23:48:22 +0100

    Implemented analysis in background_subtration.py as intended. Additional file in tests generated, but will be deleted asap.

--- tests/background_subtration.py
+++ tests/background_subtration.py
@@ -2,26 +2,33 @@
 
 from panter.core.dataloaderPerkeo import DLPerkeo
 from panter.core.corrPerkeo import corrPerkeo
+import panter.core.evalPerkeo as eP
+import panter.config.evalFitSettings as eFS
 
 dir = "/mnt/sda/PerkeoDaten1920/cycle201/cycle201/"
 dataloader = DLPerkeo(dir)
 dataloader.auto()
 
-meas = dataloader.ret_filt_meas(["cyc_no"], [194070])[0]
-print(meas())
-
-file = meas()[2]
-print(file)
+meas = dataloader.ret_filt_meas(["cyc_no"], [194070])
 
 corr_class = corrPerkeo(meas)
 corr_class.corrections["Pedestal"] = False
 corr_class.corrections["RateDepElec"] = False
-# corr_class.corr()
+corr_class.corr(bstore=True)
+
+for hist in corr_class.histograms[0, 0]:
+    fitclass = eP.DoFit(hist.hist)
+    fitclass.setup(eFS.pol0)
+    fitclass.limitrange([45000.0, 50000.0])
+
+    fitres = fitclass.fit()
+    print(fitres.fit_report())
+
+exit()
 
 import panter.core.dataPerkeo as dP
 
+file = meas[0]()[2]
+print(file)
 data = dP.RootPerkeo(file[0])
 data.auto()
-
-
-exit()
