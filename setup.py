7491a0e1e4756405ef058656522c9f13e7a293d2
Max Lamparth <max.lamparth@tum.de>
2020-12-03 13:54:37 +0100

    Created new project structure and necessary release files.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="panter",
+    version="0.0.1",
+    author="Max Lamparth",
+    author_email="max.lamparth@tum.de",
+    description="panter - Perkeo ANalysis Tool for Evaluation and Reduction",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.lrz.de/ge39dat/panter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
