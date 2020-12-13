104a894cdfaec1d8416d3b0bd34b78cd1610c93c
Max Lamparth <max.lamparth@tum.de>
2020-12-13 17:38:31 +0100

    Created new unittest filter.py with root macro filter.cpp in panter/tests for testing histograms from Perkeo root files and the filtering. Unittest tested.

---
+++ tests/filter.cpp
@@ -0,0 +1,38 @@
+
+
+int filter(const char* Sfile, const char* Outfile, int hpar0 = 10, int hpar1 = 0, int hpar2 = 10, bool bfilter = 0){
+
+    //Import file
+	TFile* fsourceS = new TFile(Sfile);
+	if (!fsourceS->IsOpen()) {
+		cerr << "Error: Cannot open file " << Sfile << endl;
+	}
+	std::cout << Sfile << std::endl;
+
+    TH1D* hist1 = new TH1D("hist1", "hist1", hpar0, hpar1, hpar2);
+    hist1->Sumw2();
+
+    TTree* dataTree = (TTree*) fsourceS->Get("dataTree");
+
+    const char* pmt0 = "(PMT[0][1] - PMT[0][0])>>hist1";
+    if (bfilter){
+        const char* filter = "DeltaTriggerTime >= 380e3 && DeltaTriggerTime < 650e3";
+        dataTree->Draw(pmt0, filter);
+    }
+    else{
+        dataTree->Draw(pmt0);
+    }
+
+    hist1->Print("all");
+    //hist1->Draw();
+
+    ofstream myfile;
+    myfile.open (Outfile);
+    for(int i = 1; i < hpar0 +1; ++i){
+        myfile << hist1->GetBinCenter(i) << "\t" << hist1->GetBinContent(i) << "\t" << hist1->GetBinError(i) << "\n";
+    }
+    myfile.close();
+
+    gApplication->Terminate();
+    return 0;
+}
---
+++ tests/filter.py
@@ -0,0 +1,56 @@
+""""""
+
+import numpy as np
+import panter.core.dataPerkeo as dP
+
+from tests.unittestroot import UnitTestRoot
+
+
+class HistTestFilter(UnitTestRoot):
+    """"""
+
+    def __init__(self, txtfile: str, params: list, root_macro: str):
+        super().__init__(
+            test_label="HistTestFilter", params=params, root_macro=root_macro
+        )
+        self.txtfile = txtfile
+        self.bfilter = self.params[3]
+
+    def do_panter(self):
+
+        data = dP.RootPerkeo(self.txtfile)
+        data.info()
+        if self.bfilter:
+            data.set_filtdef()
+            data.datafilter["DeltaTriggerTime"].active = True
+            data.datafilter["DeltaTriggerTime"].upperlimit = 380e3
+            data.datafilter["DeltaTriggerTime"].lowerlimit = 650e3
+            data.auto(1)
+        else:
+            data.auto()
+
+        hist_par = {
+            "bin_count": self.params[0],
+            "low_lim": self.params[1],
+            "up_lim": self.params[2],
+        }
+        data.gen_hist(lpmt=range(data.no_pmts), cust_hist_par=hist_par)
+        hpanter1 = data.hists[0]
+
+        return hpanter1.hist.to_numpy().flatten()
+
+    def do_root(self):
+        return super().do_root([self.txtfile], self.params)
+
+
+dir = "/mnt/sda/PerkeoDaten1920/cycle201/cycle201/"
+file = dir + "data185836-70086_beam.root"
+par1 = [3, 0, 500, 0]
+par2 = [3, 0, 500, 1]
+root_mac = "filter.cpp"
+
+test_wo_filter = HistTestFilter(txtfile=file, params=par1, root_macro=root_mac)
+test_wo_filter.test(brel_dev=False, bprint=True)
+
+test_wi_filter = HistTestFilter(txtfile=file, params=par2, root_macro=root_mac)
+test_wi_filter.test(brel_dev=False, bprint=True)
