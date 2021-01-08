a415f1cfbcd22dc9e3d85e23b222ef0fe671e336
Max Lamparth <max.lamparth@tum.de>
2021-01-09 00:05:44 +0100

    Implemented trigger analysis over several files with corrections and background subtraction in dump.py. Will be merged with trigger.py soon.

--- panter/application/dump.py
+++ panter/application/dump.py
@@ -3,72 +3,89 @@
 import panter.core.dataPerkeo as dP
 import panter.core.evalPerkeo as eP
 import panter.config.evalFitSettings as eFS
+from panter.core.corrPerkeo import corrPerkeo
 from panter.core.dataloaderPerkeo import DLPerkeo
 
 
 dir = "/mnt/sda/PerkeoDaten1920/cycle201/cycle201/"
 dataloader = DLPerkeo(dir)
 dataloader.auto()
-hist_trigger = [None]*2
+hist_trigger = [None] * 2
 
 for primary_detector in [0, 1]:
     master_both = None
     master_onlysec = None
     det_prim = primary_detector
     det_sec = 1 - det_prim
+
     for i in range(50):
         meas = dataloader[100 + i]
-        data = dP.RootPerkeo(meas.file_list[0])
-        data.info()
 
-        data.set_filtdef()
-        data.set_filt(
-            "data",
-            fkey="CoinTime",
-            active=True,
-            ftype="num",
-            low_lim=0,
-            up_lim=4e9,
-            index=det_sec,
+        corr_class = corrPerkeo(meas)
+        corr_class.corrections["Pedestal"] = True
+        corr_class.corrections["RateDepElec"] = True
+        corr_class.addition_filters.append(
+            {
+                "tree": "data",
+                "fkey": "CoinTime",
+                "active": True,
+                "ftype": "num",
+                "low_lim": 0,
+                "up_lim": 4e9,
+                "index": det_sec,
+            }
         )
-        data.set_filt(
-            "data",
-            fkey="CoinTime",
-            active=True,
-            ftype="num",
-            low_lim=4e9,
-            up_lim=4e10,
-            index=det_prim,
+        corr_class.addition_filters.append(
+            {
+                "tree": "data",
+                "fkey": "CoinTime",
+                "active": True,
+                "ftype": "num",
+                "low_lim": 4e9,
+                "up_lim": 4e10,
+                "index": det_prim,
+            }
         )
-        data.auto(1)
-        data.gen_hist([])
-        hist_onlysec = data.hist_sums
+        corr_class.corr(bstore=True, bwrite=False)
+        hist_onlysec = corr_class.histograms[0][0]
 
-        data.set_filtdef()
-        data.set_filt(
-            "data",
-            fkey="CoinTime",
-            active=True,
-            ftype="num",
-            low_lim=0,
-            up_lim=4e9,
-            index=det_sec,
+        # TODO: make clear function for reusability
+        corr_class.addition_filters = []
+        corr_class.histograms = []
+        corr_class.addition_filters.append(
+            {
+                "tree": "data",
+                "fkey": "CoinTime",
+                "active": True,
+                "ftype": "num",
+                "low_lim": 0,
+                "up_lim": 4e9,
+                "index": det_sec,
+            }
         )
-        data.set_filt(
-            "data",
-            fkey="CoinTime",
-            active=True,
-            ftype="num",
-            low_lim=0,
-            up_lim=4e9,
-            index=det_prim,
+        corr_class.addition_filters.append(
+            {
+                "tree": "data",
+                "fkey": "CoinTime",
+                "active": True,
+                "ftype": "num",
+                "low_lim": 0,
+                "up_lim": 4e9,
+                "index": det_prim,
+            }
         )
-        data.set_filt(
-            "data", fkey="Detector", active=True, ftype="bool", rightval=det_prim
+        corr_class.addition_filters.append(
+            {
+                "tree": "data",
+                "fkey": "Detector",
+                "active": True,
+                "ftype": "bool",
+                "rightval": det_prim,
+            }
         )
-        data.auto(1)
-        data.gen_hist([])
-        hist_both = data.hist_sums
+        corr_class.corr(bstore=True, bwrite=False)
+        hist_both = corr_class.histograms[0][0]
+
         hist_onlysec[det_prim].addhist(hist_both[det_prim])
 
         if i == 0:
@@ -80,7 +97,7 @@ for primary_detector in [0, 1]:
 
     master_both.divbyhist(master_onlysec)
     hist_trigger[det_prim] = master_both
-    # hist_trigger.plt([0, 12e3, 0, 1])
+    # hist_trigger[det_prim].plt([0, 12e3, 0, 1])
 
 for hist in hist_trigger:
     fitclass = eP.DoFit(hist.hist)
@@ -95,8 +112,12 @@ for hist in hist_trigger:
     fitclass.fit()
 
 """
-a:  0.00275708 +/- 4.0498e-05 (1.47%) (init = 0.003)
-p:  0.70621967 +/- 0.00863997 (1.22%) (init = 0.655)
-a:  0.00325521 +/- 7.5923e-05 (2.33%) (init = 0.003)
-p:  0.61569591 +/- 0.01218149 (1.98%) (init = 0.655)
+nocorr:
+a:  0.00253116 +/- 4.5523e-05 (1.80%) (init = 0.003)
+p:  0.73623919 +/- 0.01119634 (1.52%) (init = 0.655)
+a:  0.00287972 +/- 7.5364e-05 (2.62%) (init = 0.003)
+p:  0.68840456 +/- 0.01481232 (2.15%) (init = 0.655)
+
+corr:
+
 """
