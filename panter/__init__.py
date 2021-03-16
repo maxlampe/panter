bf904b3011b4ee948865d8b65732292818d854fc
Max Lamparth <max.lamparth@tum.de>
2021-03-16 20:58:13 +0100

    Added automated output dir creation, should it not exist. in panter/panter/__init__.py

--- panter/__init__.py
+++ panter/__init__.py
@@ -1,4 +1,10 @@
 import os
 
 output_path = os.path.dirname(os.path.realpath(__file__)) + "/output"
-assert os.path.exists(output_path), "ERROR: Directory panter/output is missing."
+if not os.path.exists(output_path):
+    try:
+        os.mkdir(output_path)
+    except OSError:
+        print("Creation of the directory %s failed" % output_path)
+    else:
+        print("Successfully created the directory %s " % output_path)
