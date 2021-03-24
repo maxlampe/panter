9ac810692432621e82d9623e98c47a9c96b81fd3
Max Lamparth <max.lamparth@tum.de>
2021-03-24 18:32:51 +0100

    style: A few pep8-related changes.

--- panter/application/pedestal_diffsources.py
+++ panter/application/pedestal_diffsources.py
@@ -7,21 +7,23 @@ import matplotlib.dates as md
 import datetime
 from ped_map import PedMapPerkeo
 
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
+pd.set_option("display.max_rows", 500)
+pd.set_option("display.max_columns", 500)
+pd.set_option("display.width", 1000)
 
 PMT = 1
-dates = np.asarray([
-    # Allmode
-    datetime.datetime(2020, 1, 19, 18, 10).timestamp(),
-    # Bothmode
-    datetime.datetime(2020, 1, 20, 7, 10).timestamp(),
-    # Allmode
-    datetime.datetime(2020, 1, 23, 12, 30).timestamp(),
-    # Bothmode
-    datetime.datetime(2020, 1, 23, 20, 45).timestamp(),
-])
+dates = np.asarray(
+    [
+        # Allmode
+        datetime.datetime(2020, 1, 19, 18, 10).timestamp(),
+        # Bothmode
+        datetime.datetime(2020, 1, 20, 7, 10).timestamp(),
+        # Allmode
+        datetime.datetime(2020, 1, 23, 12, 30).timestamp(),
+        # Bothmode
+        datetime.datetime(2020, 1, 23, 20, 45).timestamp(),
+    ]
+)
 
 
 sources = np.asarray(["beam", "ce", "cd", "cs", "bi", "sn"])
@@ -33,7 +35,7 @@ axs[0].set_title("Pedestals over time Det 0")
 axs[1].set_title("Peak pos over time Det 1")
 axs.flat[0].set(xlabel="Time [s]", ylabel="Ped pos [ch]")
 axs.flat[1].set(xlabel="Time [s]", ylabel="Ped pos [ch]")
-xfmt = md.DateFormatter('%m-%d\n%H:%M')
+xfmt = md.DateFormatter("%m-%d\n%H:%M")
 axs[0].xaxis.set_major_formatter(xfmt)
 axs[1].xaxis.set_major_formatter(xfmt)
 plt.tight_layout()
@@ -54,7 +56,7 @@ for src in sources:
 
     # print((ped_err_df == 0).sum())
     dataf[src] = (ped_df / ped_err_df ** 2).sum() / (1.0 / ped_err_df ** 2).sum()
-    dataf[src + "_err"] = np.sqrt(1. / (1.0 / ped_err_df ** 2).sum())
+    dataf[src + "_err"] = np.sqrt(1.0 / (1.0 / ped_err_df ** 2).sum())
     dates_plot = [datetime.datetime.fromtimestamp(t) for t in df_filt["time"]]
 
     axs[0].errorbar(
@@ -80,7 +82,9 @@ plt.show()
 dataf_diff = pd.DataFrame()
 for src in sources[1:]:
     dataf_diff[f"bm_{src}"] = dataf["beam"] - dataf[src]
-    dataf_diff[f"bm_{src}_err"] = np.sqrt((dataf["beam_err"] ** 2 + dataf[f"{src}_err"] ** 2))
+    dataf_diff[f"bm_{src}_err"] = np.sqrt(
+        (dataf["beam_err"] ** 2 + dataf[f"{src}_err"] ** 2)
+    )
 
 print("\nWAM Pedestals for different sources for each PMT:\n")
 print(dataf)
@@ -88,9 +92,3 @@ print("\nShift of pedestal compared to beam for each PMT:\n")
 print(dataf_diff)
 print("\nShift on DetSum compared to beam pedestal:\n")
 print(dataf_diff.sum())
-
-
-
-
-
-
