c5ef2da0e9fe0a68eb5ac57061c75f492c1ee29a
Max Lamparth <max.lamparth@tum.de>
2021-04-15 14:12:45 +0200

    feat: Toying around with the PMT charge spectrum function.

--- panter/application/dump.py
+++ panter/application/dump.py
@@ -2,24 +2,24 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from panter.core.dataPerkeo import HistPerkeo
+import matplotlib.pyplot as plt
+from panter.core.dataPerkeo import HistPerkeo, RootPerkeo
 import panter.core.evalPerkeo as eP
 from panter.core.evalFunctions import charge_spec
 
-x = np.linspace(0., 10000., num=1000)
+x = np.linspace(0.0, 10000.0, num=1000)
 params = {
     "w": 0.3,
     "a": 0.003,
     "lam": 3.4,
-    "q0": 2.,
+    "q0": 2.0,
     "sig0": 0.9,
-    "c0": 10.,
-    "sig": 4.,
-    "mu": 5.,
+    "c0": 10.0,
+    "sig": 1.0,
+    "mu": 5.0,
     "k_max": 100,
 }
 
 val = charge_spec(x, **params)
 plt.plot(x, val)
 plt.show()
-
