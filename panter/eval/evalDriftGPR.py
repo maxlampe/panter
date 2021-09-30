8fcae44a75f698598bf5f7a2f89e3ad961465ff2
Max Lamparth <max.lamparth@tum.de>
2021-09-30 16:27:24 +0200

    feat: Created new evaluation script evalDriftGPR.py to use Gaussian Process Regression to smoothen and infer from drift factor data to enable continuous drift correction.

---
+++ panter/eval/evalDriftGPR.py
@@ -0,0 +1,180 @@
+""""""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import torch
+import pyro
+import pyro.contrib.gp as gp
+import pyro.distributions as dist
+
+from panter.config import conf_path
+from panter.map.drift_dettsum_map import DriftDetSumMapPerkeo
+
+assert pyro.__version__.startswith("1.7.0")
+pyro.set_rng_seed(0)
+
+
+class GPRDriftData:
+    """"""
+
+    def __init__(self, n_data_steps: int = None):
+        self._n_data_steps = n_data_steps
+        self.pdm = DriftDetSumMapPerkeo(np.array([]), bimp_detsum=True, bimp_sn=False)
+        self.pdm()
+
+        self.df = self.pdm.maps[0]
+        self.data_form = {"a": None, "b": None}
+        self.x, self.y0, self.y1 = self._preprocess_data()
+
+    def __call__(self, *args, **kwargs):
+        return {"time": self.x, "det0": self.y0, "det1": self.y1}
+
+    def _preprocess_data(self):
+        """Inplace modification"""
+
+        for index, row in self.df.iterrows():
+            if pd.isnull(row["pmt_fac"][1]):
+                self.df.drop([index], inplace=True)
+
+        # Normalize data
+        x_raw = np.array(self.df["time"].apply(pd.Series))
+        self.data_form["a"] = x_raw[0]
+        self.data_form["b"] = x_raw[-1] - x_raw[0]
+
+        x_raw = self.timestamp_to_data(x_raw)
+        y_raw0 = np.array(self.df["pmt_fac"].apply(pd.Series)[0])
+        y_raw1 = np.array(self.df["pmt_fac"].apply(pd.Series)[1])
+
+        x = torch.flatten(torch.tensor(x_raw[:: self._n_data_steps]))
+        y0 = torch.tensor(y_raw0[:: self._n_data_steps])
+        y1 = torch.tensor(y_raw1[:: self._n_data_steps])
+
+        return x, y0, y1
+
+    def data_to_timestamp(self, x: torch.tensor):
+        """"""
+        return x * self.data_form["b"] + self.data_form["a"]
+
+    def timestamp_to_data(self, x: torch.tensor):
+        """"""
+        return (x - self.data_form["a"]) / self.data_form["b"]
+
+
+class GPRDrift:
+    def __init__(self, detector: int = 0):
+        self._det = detector
+        self.dataclass = GPRDriftData()
+        self.data = self.dataclass()
+        self.gpr = self._create_gpr_model(
+            x_train=self.data["time"], y_train=self.data[f"det{self._det}"]
+        )
+        # ^ minimum for enable loading of state dict
+
+        self.losses = []
+
+    def __call__(self, x_eval: torch.tensor):
+        """"""
+
+        self.gpr.eval()
+        x_eval = torch.tensor(x_eval, dtype=torch.float64)
+
+        with torch.no_grad():
+            mean, cov = self.gpr(x_eval, full_cov=True, noiseless=False)
+        sd = cov.diag().sqrt()
+
+        return mean, sd
+
+    def train(self, n_steps: int = 5000):
+        """"""
+
+        self.gpr.train()
+
+        optimizer = torch.optim.Adam(self.gpr.parameters(), lr=0.005)
+        loss_fn = pyro.infer.Trace_ELBO().differentiable_loss
+        for i in range(n_steps):
+            optimizer.zero_grad()
+            loss = loss_fn(self.gpr.model, self.gpr.guide)
+            loss.backward()
+            optimizer.step()
+            self.losses.append(loss.item())
+
+    def plot_results(self, n_test: int = 500):
+        """"""
+
+        plt.figure(figsize=(12, 6))
+        x_data = self.data["time"]
+        y_data = self.data[f"det{self._det}"]
+        plt.plot(x_data.numpy(), y_data.numpy(), "kx")
+        x_test = torch.linspace(
+            x_data.numpy().min(), x_data.numpy().max(), n_test, dtype=torch.float64
+        )
+
+        mean, sd = self(x_test)
+
+        plt.plot(x_test.numpy(), mean.numpy(), "r", lw=2)  # plot the mean
+        plt.fill_between(
+            x_test.numpy(),  # plot the two-sigma uncertainty about the mean
+            (mean - 2.0 * sd).numpy(),
+            (mean + 2.0 * sd).numpy(),
+            color="C0",
+            alpha=0.3,
+        )
+        plt.show()
+
+    def plot_losses(self):
+        """"""
+        plt.plot(self.losses)
+        plt.show()
+
+    def save_model(self, file_name: str = "gpr_model.plk"):
+        """"""
+        torch.save(self.gpr.state_dict(), file_name)
+
+    def load_model(self, file_name: str = "gpr_model.plk"):
+        """"""
+        self.gpr.load_state_dict(torch.load(file_name))
+
+    @staticmethod
+    def _create_gpr_model(x_train: torch.tensor, y_train: torch.tensor):
+        """"""
+
+        pyro.clear_param_store()
+        kernel = gp.kernels.Matern52(
+            input_dim=1,
+            variance=torch.tensor(1.0),
+            lengthscale=torch.tensor(0.01),
+        )
+        gpr = gp.models.GPRegression(
+            X=x_train,
+            y=y_train,
+            kernel=kernel,
+            noise=torch.tensor(1.0),
+        )
+
+        gpr.kernel.lengthscale = pyro.nn.PyroSample(dist.LogNormal(0.0, 1.0))
+        gpr.kernel.variance = pyro.nn.PyroSample(dist.LogNormal(0.0, 1.0))
+
+        return gpr
+
+
+def main(bcalc_anew: bool = False):
+
+    for det in [0, 1]:
+        gpr_class = GPRDrift(detector=det)
+
+        if bcalc_anew:
+            gpr_class.train()
+            gpr_class.save_model(file_name=f"{conf_path}/gpr_model_det{det}.plk")
+
+        else:
+            gpr_class.load_model(file_name=f"{conf_path}/gpr_model_det{det}.plk")
+
+        gpr_class.plot_results()
+        res = gpr_class(torch.tensor([0.1, 0.3, 0.8]))
+        print(res)
+
+
+if __name__ == "__main__":
+    main(bcalc_anew=False)
